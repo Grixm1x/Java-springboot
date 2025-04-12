85a547229c35ecb414075309913555d40bbcb156
Joao Marcelus <joaomarcelus6@gmail.com>
2025-04-12 10:06:37 -0300

    registro de componentes

--- src/main/java/com/joaomarcelus/course/resouces/UserResource.java
+++ src/main/java/com/joaomarcelus/course/resouces/UserResource.java
@@ -1,19 +1,31 @@
 package com.joaomarcelus.course.resouces;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaomarcelus.course.entities.User;
+import com.joaomarcelus.course.services.UserService;
 
 @RestController 
 @RequestMapping(value = "/users")
 public class UserResource {
-
+	
+	@Autowired
+	private UserService service;
 	@GetMapping
-	public ResponseEntity<User> returnUser(){
-		User u = new User(1L, "joao Marcelus", "joaomarcelus6@gmail.com","61984848","12345" );
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> returnUser(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> returnId(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);		
 	}
 }
